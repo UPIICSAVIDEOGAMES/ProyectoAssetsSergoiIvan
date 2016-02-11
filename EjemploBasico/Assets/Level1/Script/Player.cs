88e09288d5efcd3652661b6694e3d9fe40564781
sergio ivan lopez monzon <sergioivan154@gmail.com>
2016-02-10 19:49:00 -0600

    script de player

--- EjemploBasico/Assets/Level1/Script/Player.cs
+++ EjemploBasico/Assets/Level1/Script/Player.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public float data = 0;
 	// Use this for initialization
 	void Start () {
+
+		Debug.Log ("Personaje en pantalla");
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		transform.localPosition = new Vector3 (data, 3, 0);
 	}
 }
