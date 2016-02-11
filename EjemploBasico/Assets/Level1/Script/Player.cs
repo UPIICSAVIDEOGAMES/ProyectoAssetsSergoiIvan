f912baabe881b85829c9ae668c86ab42efb4025e
sergio ivan lopez monzon <sergioivan154@gmail.com>
2016-02-10 20:20:52 -0600

    Instancias otros objetos (útiles para balas)
    se muestra como se hace uso de los prefabs para ser llamados dentro de
    un gameobject

--- EjemploBasico/Assets/Level1/Script/Player.cs
+++ EjemploBasico/Assets/Level1/Script/Player.cs
@@ -4,15 +4,30 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	public float data = 0;
+	public Transform enemigo;
+	private Vector3 incremento;
+
 	// Use this for initialization
 	void Start () {
 
 		Debug.Log ("Personaje en pantalla");
+		incremento = new Vector3 (0.05f, 0, 0);
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.localPosition = new Vector3 (data, 3, 0);
+		if (transform.localPosition.x>=5.5)
+		{
+			incremento.x *= -1;
+
+			Instantiate(enemigo, transform.localPosition, Quaternion.identity); // Aparecer el tiburon cuando este en cierta posición 
+			//transform.Rotate(Vector3.right,Time.deltaTime);
+		}
+		/*if (transform.localPosition.x <= 5 ) {
+			incremento.x *= 1;
+			//transform.Rotate(Vector3.left,Time.deltaTime);
+		}*/
+		GetComponent<Transform> ().localPosition += incremento;
 	}
 }
---
+++ EjemploBasico/Assets/Level1/Script/Tiburon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tiburon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition -= new Vector3 (0.05f, 0, 0);
+	}
+}
