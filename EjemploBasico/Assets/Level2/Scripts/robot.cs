cc5552b9df1f14aded9aa8192eccb75bbcaff2eb
sergio ivan lopez monzon <sergioivan154@gmail.com>
2016-02-13 12:08:03 -0600

    ejemplo nivel 2

---
+++ EjemploBasico/Assets/Level2/Scripts/robot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class robot : MonoBehaviour {
+
+	public Transform bala;
+	private Vector3 position;
+	//private Vector3 positionBala;
+	// Use this for initialization
+	void Start () {
+		position = new Vector3 (0.05f, 0, 0);
+		//positionBala = new Vector3 (0.05f, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.localPosition.x >= 5) {
+			position.x *= -1;
+			Instantiate(bala, transform.localPosition, Quaternion.identity); 
+		}
+		else {
+			transform.localPosition += position;
+		}
+
+
+
+	}
+}
---
+++ EjemploBasico/Assets/bala.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bala : MonoBehaviour {
+
+	private Vector3 balaposition;
+	// Use this for initialization
+	void Start () {
+		balaposition = new Vector3 (0.05f, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition += balaposition;
+	}
+}
